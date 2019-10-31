ca0c6a376c7b27a7b287a467b1d04f4b0e95710a
Daniel Jaouen <dcj24@cornell.edu>
2019-10-31 00:17:29 -0400

    Add utility functions

--- lambdata_danieljaouen/df_utils.py
+++ lambdata_danieljaouen/df_utils.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 ONES = pd.Series(np.ones(20))
 ZEROS = pd.Series(np.zeros(20))
+
+
+def isna(df):
+    print(df.isna().sum())
+
+
+def correlation_matrix(df):
+    print(df.corr())
